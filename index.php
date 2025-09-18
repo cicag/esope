09e62bbc50125c2bf4d05258ea519142b5db9f97
cicag <riiiooooo13@gmail.com>
2025-09-19 04:00:07 +0700

    placeholder

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Muat fungsi dasar pencarian
+//require 'search/functions.php';
+?>
+
+<!-- Kepala Halaman -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ESOPE</title>
+</head>
+<body>
+    <h1>Astaga</h1>
+
+<!-- Kaki, tentang Bantu -->
+  <footer>
+    <a>ESOPE v0.0.1-pre-alpha</a>
+  </footer>
+
+</body>
+</html>
