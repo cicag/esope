a64b42529a66c8fa777e2e101d07921aead69744
cicag <riiiooooo13@gmail.com>
2025-09-20 22:16:04 +0700

    about app placeholder

---
+++ includes/about.php
@@ -0,0 +1,29 @@
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ESOPE</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            max-width: 800px;
+            margin: 50px auto;
+            line-height: 1.6;
+        }
+        h1 {
+            color: #2c3e50;
+        }
+        ol {
+            padding-left: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<h1>ESOPE</h1>
+
+<p><strong>ESOPE</strong> (akronim untuk <em>Esope Sistem Observasi Pasien Elektronik</em>), adalah aplikasi kalkulator NEWS sederhana. Prototipe ini dibuat untuk demonstrasi konsep ESOPE sebagai aplikasi yang membantu mengetahui inntervensi lebih lanjut, dan meminimalkan kendala dalam penghitungan manual di ruangan..</p>
+
+</body>
+</html>
