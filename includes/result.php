94f07bc7d97e0bfa4579c446de4136b3b2764a9a
cicag <riiiooooo13@gmail.com>
2025-09-21 14:20:50 +0700

    better modular

---
+++ includes/result.php
@@ -0,0 +1,13 @@
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>Kategori</th>
+      <th>Respon Klinis</th>
+      <th>Saran Tindakan</th>
+      <th>Monitoring</th>
+    </tr>
+  </table>
+
+  <!-- Tampilkan ini jika data yang dipanggil kosong -->
+  <?php if (empty($news)): ?>
+  <p>Tidak ditemukan hasil.</p>
+  <?php endif; ?>
